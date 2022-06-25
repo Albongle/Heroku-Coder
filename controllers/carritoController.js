4123853f1f8c57d1468ecdd2c8920fb9af721570
u531677 <ANBongioanni@teco.com.ar>
2022-06-25 20:10:17 -0300

    carrito controller

--- controllers/carritoController.js
+++ controllers/carritoController.js
@@ -1,22 +1,82 @@
+const carritoDAO = require("../dao/carritoDAOMongoDb");
+const gestorCarrito = new carritoDAO();
+module.exports = class CarritoController{
 
+    static async agregarProductoAlCarrito(username, producto){
+        let data;
+        if(username && producto){
+            const carritoDelUsuario = (await gestorCarrito.getElementosByParams(username,"username")).shift();
+            if(carritoDelUsuario){
+                const productoExistente = carritoDelUsuario.productos.find(p=> p.id === producto.id);
+                if(productoExistente){
+                    productoExistente.cantidad++;
+                }
+                else{
+                    carritoDelUsuario.productos.push(producto);
+                }
+                data =  await gestorCarrito.updateElemento(carritoDelUsuario._id,carritoDelUsuario);
+                }
+            else{
+                data = await gestorCarrito.addElementos({username:username,productos:producto});
+            }
+            return {status:"ok",code:200,message:`Se agrego producto al carrito`, carrito:data.shift()};
+        }
 
-async function agregarProdcutoAlCarrito(gestorCarrito,username, producto){
-let respuesta;
-const productosUsuario = (await gestorCarrito.getElementosByParams(username,"username")).shift();
-    if(productosUsuario){
-        const productoExistente = productosUsuario.productos.find(p=> p.id === producto.id);
-        if(productoExistente){
-            productoExistente.cantidad++;
-        }
-        else{
-            productosUsuario.productos.push(producto);
-        }
-        respuesta =  await gestorCarrito.updateElemento(productosUsuario._id,productosUsuario);
+        return {status:"error",code:404,message:`No se recibieron algunos de los datos necesarios para el alta`};
     }
-    else{
-        respuesta = await gestorCarrito.addElementos({username:username,productos:producto});
+
+    static async borrarCarritoPorId(id){
+
+        if(id){
+            const data =  await gestorCarrito.deleteElementoById(id);
+            if(data.length>0){
+                return {status:"ok",code:200,message:`Se elimino el carrito con id ${id}`, carrito:data};
+            }
+            return {status:"error",message:'Carrito no encontrado', code:406};
+        }
+        return {status:"error",code:404,message:`No se recibieron algunos de los datos necesarios`};
     }
-    return respuesta.shift();
+
+    static async obtenerProductosDeUnCarrito(id){
+        if(id){
+            const data =  await gestorCarrito.getElementoById(id);
+            if(data != null){
+                return {status:"ok",code:200,message:`Solicitud procesada exitosamente`, carrito:data};
+            }
+            return {status:"error",message:'Carrito no encontrado', code:406};
+        }
+        return {status:"error",code:404,message:`No se recibieron algunos de los datos necesarios`};
+    }
+
+    static async actualizarProductosDeUnCarrito(id, objeto){
+        if(id && objeto){
+            const data = await gestorCarrito.updateElemento(id, objeto);
+            if(data.length>0){
+                return {status:"ok",code:200,message:`Se actualizo el carrito con id ${id}`, carrito:data};
+            }
+            return {status:"error",message:'Carrito no encontrado', code:406};
+        }
+        return {status:"error",code:404,message:`No se recibieron algunos de los datos necesarios`};
+    }
+    static async borraUnProductoDeUnCarrito(idCarrito, idProducto){
+
+        if(idCarrito && idProducto){
+            const carrito = await gestorCarrito.getElementoById(idCarrito);
+
+            if(carrito != null){
+                let indice = carrito.productos.findIndex(p=> p.id === idProducto);
+                if(indice>0){
+                    const productosAux = carrito.productos.splice(indice,1);
+                    const data = await gestorCarrito.updateElemento(idCarrito,{username:req.session.passport.user.username,productos:productosAux});
+                    return {status:"ok",code:200,message:`Se elimino el producto id ${idProducto}, del carrito con id ${idCarrito}`, carrito:data};
+                }
+                return {status:"error",message:'Producto no econtrado en el carrito', code:406};
+            }
+            return {status:"error",message:'Carrito no encontrado', code:406};
+        }
+        return {status:"error",code:404,message:`No se recibieron algunos de los datos necesarios`};
+    }
+
 }
 
-module.exports = agregarProdcutoAlCarrito;
+
--- routes/apiCarrito.js
+++ routes/apiCarrito.js
@@ -1,65 +1,41 @@
 const express = require("express");
-const agregarProdcutoAlCarrito = require("../controllers/carritoController");
 const router = express.Router();
-const carritoDAO = require("../dao/carritoDAOMongoDb");
+const CarritoController = require("../controllers/carritoController");
 const mdw = require("../middlewares/middlewares");
-const gestorCarrito = new carritoDAO();
+
 
 
 router.post("",mdw.validarSession, async (req, res)=>{
-    const objeto = req.body;
-    const respuesta = await agregarProdcutoAlCarrito(gestorCarrito,req.session.passport.user.username,objeto);
-    
-    req.app.io.sockets.emit("refresh-carrito",{carrito:respuesta});
-    res.status(200).json({carrito:respuesta});
+    const producto = req.body;
+    const respuesta = await CarritoController.agregarProductoAlCarrito(req.session.passport.user.username, producto);
+    req.app.io.sockets.emit("refresh-carrito",{carrito:respuesta.carrito});
+    res.status(respuesta.code).json(respuesta);
 
 });
 router.delete("/:id",mdw.validarSession, async (req, res)=>{
     let {id}=req.params;
-    if(await gestorCarrito.deleteElementoById(id)){
-        res.status(200).json({status:"ok", message:`Se elimino el carrito con id ${id}`});
-    }else{
-        res.status(406).json({error:'Carrito no encontrado'});
-    }
+    const respuesta = await CarritoController.borrarCarritoPorId(id);
+    res.status(respuesta.code).json(respuesta);
 });
 router.get("/:id/productos",mdw.validarSession, async (req, res)=>{
     let {id}=req.params;
-    const respuesta = await gestorCarrito.getElementoById(id);
-    if(respuesta != null){
+    const respuesta = await CarritoController.obtenerProductosDeUnCarrito(id);
+    res.status(respuesta.code).json(respuesta);
 
-        res.status(200).json(respuesta.productos);
-    }else{
-        res.status(406).json({error:'Carrito no encontrado'});
-    }
 });
 router.post("/:id/productos",mdw.validarSession,async (req, res)=>{
 
     let {id}=req.params;
     const objeto = req.body;
-    if(await gestorCarrito.updateElemento(id, {username:req.session.passport.user.username,productos:objeto})){
-        res.status(200).json({status:"carrito actualizado"});
-    }else{
-        res.status(406).json({error:'Carrito no encontrado'});
-    }
+    const respuesta = await CarritoController.actualizarProductosDeUnCarrito(id, {username:req.session.passport.user.username,productos:objeto});
+    res.status(respuesta.code).json(respuesta);
 
 });
-router.delete("/:id/productos/:id_productos",mdw.validarSession,async (req, res)=>{
+router.delete("/:idCarrito/productos/:idProducto",mdw.validarSession,async (req, res)=>{
 
-    let {id,id_productos}=req.params;
-    const respuesta = await gestorCarrito.getElementoById(id);
-    if(respuesta != null){
-        let indice = respuesta.productos.findIndex(x=> x._id=== id_productos);
-        if(indice>0){
-            const resultado = respuesta.productos.splice(indice,1);
-            await gestorCarrito.updateElemento(id,{username:req.session.passport.user.username,productos:resultado});
-            res.status(200).json({status:"ok", message:"carrito actualizado"});
-        }else{
-            res.status(406).json({error:'Producto no encontrado en el carrito'});
-        }
-    }else{
-        res.status(406).json({error:'Carrito no encontrado'});
-    }
-
+    let {idCarrito,idProducto}=req.params;
+    const respuesta  = await CarritoController.borraUnProductoDeUnCarrito(idCarrito, idProducto);
+    res.status(respuesta.code).json(respuesta);
 });
 
 
