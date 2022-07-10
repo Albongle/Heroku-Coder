fef88152243c46419832726b8b3a1720d7ffea22
u531677 <ANBongioanni@teco.com.ar>
2022-07-10 12:58:58 -0300

    correcciones

--- repository/Producto.Repository.js
+++ repository/Producto.Repository.js
@@ -1,5 +1,4 @@
 const ProductoMapper = require("../mapper/Producto.Mapper");
-const CustomError = require("../model/Error.Model");
 const Producto = require("../model/Producto.Model");
 module.exports = class ProductoRepository{
 
